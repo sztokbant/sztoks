e788036dc82c9e85ed1c6b0a535eb4c42ee81fd0
Eduardo Sztokbant <du@du.net.br>
2020-06-24 10:51:01 -0700

    Hello World!

---
+++ src/test/java/br/net/du/myequity/MainTests.java
@@ -0,0 +1,13 @@
+package br.net.du.myequity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
