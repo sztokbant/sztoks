9256ab30a9d640d787f23d71f11bfd88d4de288b
Eduardo Sztokbant <sztokbant@posteo.net>
2023-04-08 15:34:58 -0700

    Add unit-test for no CSRF token to SnapshotDeleteControllerTest

--- src/test/java/br/net/du/sztoks/controller/SnapshotDeleteControllerTest.java
+++ src/test/java/br/net/du/sztoks/controller/SnapshotDeleteControllerTest.java
@@ -3,9 +3,11 @@ package br.net.du.sztoks.controller;
 import static br.net.du.sztoks.test.ModelTestUtils.SNAPSHOT_ID;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
@@ -13,6 +15,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 @SpringBootTest
@@ -33,15 +36,31 @@ class SnapshotDeleteControllerTest extends SnapshotControllerPostTestBase {
     }
 
     @Test
-    public void post_happy() throws Exception {
+    public void post_noCsrf_forbidden() throws Exception {
         // WHEN
-        mvc.perform(
-                MockMvcRequestBuilders.post(url)
-                        .with(csrf())
-                        .with(user(user.getEmail()))
-                        .contentType(MediaType.APPLICATION_FORM_URLENCODED));
+        final ResultActions resultActions =
+                mvc.perform(
+                        MockMvcRequestBuilders.post(url)
+                                .with(user(user.getEmail()))
+                                .contentType(MediaType.APPLICATION_FORM_URLENCODED));
 
         // THEN
+        resultActions.andExpect(status().isForbidden());
+        verifyNoInteractions(snapshotService);
+    }
+
+    @Test
+    public void post_happy() throws Exception {
+        // WHEN
+        final ResultActions resultActions =
+                mvc.perform(
+                        MockMvcRequestBuilders.post(url)
+                                .with(csrf())
+                                .with(user(user.getEmail()))
+                                .contentType(MediaType.APPLICATION_FORM_URLENCODED));
+
+        // THEN
+        resultActions.andExpect(status().is3xxRedirection());
         verify(snapshotService).deleteSnapshot(eq(user), eq(snapshot));
     }
 }
